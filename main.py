3b654aeae38a15bbcdddf313ffdba44e1180a670
nlpcui <penncui@foxmail.com>
2022-07-17 18:53:50 +0200

    add gpu

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys, torch, os, json, configparser, argparse
+import sys, torch, os, json, configparser, argparse, datetime
 from tqdm import tqdm
 from process_data import DuolingoDatasetBuilder, DuolingoDataset, DuolingoTokenizer
 from torch.utils.data import Dataset, DataLoader
-from transformers import AdamW
+from torch.optim import AdamW
 from models import SAKT
 import torch.nn as nn
 from transformers import get_linear_schedule_with_warmup
@@ -10,8 +10,17 @@ from pprint import pprint
 from sklearn.metrics import roc_auc_score, f1_score, precision_score, recall_score, accuracy_score
 
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if device == 'cuda':
+    print('using gpu {}'.format(torch.cuda.get_device_name(0)))
+else:
+    print('using cpu')
+
 assistment_dir = '/cluster/work/sachan/pencui/ASSISTment'
-duolingo_en_es_dir = '/Users/cuipeng/Documents/Datasets/duolingo_2018_shared_task/data_en_es' #"/cluster/work/sachan/pencui/duolingo_2018_shared_task/data_en_es"
+duolingo_raw_en_es_dir = "/cluster/work/sachan/pencui/duolingo_2018_shared_task/data_en_es" # '/Users/cuipeng/Documents/Datasets/duolingo_2018_shared_task/data_en_es'
+duolingo_processed_en_es_dir = "/cluster/project/sachan/pencui/duolingo/data_en_es/"
+duolingo_en_es_models_dir = '/cluster/work/sachan/pencui/duolingo_2018_shared_task/data_en_es/models'
+
 file_paths = {
     "Assistment_2004_2005": 'ASSISTment_2004_2005/ds92_tx_All_Data_172_2016_0504_081852.txt',
     "Assistment_2005_2006": 'ASSISTment_2005_2006/ds120_tx_All_Data_265_2017_0414_065125.txt',
@@ -37,9 +46,9 @@ def cal_metrics(logits, y_labels):
     y_pos_probs = nn.functional.softmax(logits, dim=-1)[:,1]
     y_pred = torch.argmax(logits, dim=-1)
 
-    y_labels_selected = torch.masked_select(y_labels, valid_positions).detach().numpy()
-    y_pos_probs_selected = torch.masked_select(y_pos_probs, valid_positions).detach().numpy()
-    y_pred_labels_selected = torch.masked_select(y_pred, valid_positions).detach().numpy()
+    y_labels_selected = torch.masked_select(y_labels, valid_positions).numpy()
+    y_pos_probs_selected = torch.masked_select(y_pos_probs, valid_positions).numpy()
+    y_pred_labels_selected = torch.masked_select(y_pred, valid_positions).numpy()
 
     auc = roc_auc_score(y_true=y_labels_selected, y_score=y_pos_probs_selected)
     f1 = f1_score(y_true=y_labels_selected, y_pred=y_pred_labels_selected)
@@ -50,6 +59,10 @@ def cal_metrics(logits, y_labels):
     return round(auc, 4), round(precision, 4), round(recall, 4), round(f1, 4), round(accuracy, 4)
 
 
+def eval(args):
+    pass
+
+
 def train(args):
     # dataset_builder = DuolingoDatasetBuilder(
     #     filepath=os.path.join(args.data_dir, args.data_file), 
@@ -58,9 +71,9 @@ def train(args):
     # )
     # dataset_builder.save_by_stu(os.path.join(duolingo_en_es_dir, file_paths['Duolingo_en_es_train_processed']))
     # exit(1)
-    dataset = DuolingoDataset(os.path.join(duolingo_en_es_dir, file_paths['Duolingo_en_es_train_processed']), shuffle=True)
+    dataset = DuolingoDataset(os.path.join(duolingo_processed_en_es_dir, file_paths['Duolingo_en_es_train_processed']), shuffle=True)
     tokenizer = DuolingoTokenizer(args.vocab_save_path)
-    dataloader = DataLoader(dataset, batch_size=args.batch_size)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, drop_last=True)
 
     model = SAKT(
         num_emb=len(tokenizer), 
@@ -78,7 +91,9 @@ def train(args):
         dim_ff_interaction=args.dim_ff_interaction, 
         dropout_interaction=args.dropout_interaction, 
         num_label=args.num_label
-    )
+    ).to(device)
+    
+    model.train()
 
     total_steps = len(dataset) // args.batch_size * args.epoch
 
@@ -115,25 +130,25 @@ def train(args):
             x_interaction_attn_mask = x_interaction_attn_mask.unsqueeze(1).repeat(1, args.num_attn_heads_interaction, 1, 1).view(args.batch_size*args.num_attn_heads_interaction, args.max_len, -1)
 
             logits = model(
-                x_exercise=x_exercise,
-                x_interaction=x_interaction,
-                x_exercise_attn_mask=x_exercise_attn_mask,
-                x_interaction_attn_mask=x_interaction_attn_mask,
-                y_labels=y_labels
+                x_exercise=x_exercise.to(device),
+                x_interaction=x_interaction.to(device),
+                x_exercise_attn_mask=x_exercise_attn_mask.to(device),
+                x_interaction_attn_mask=x_interaction_attn_mask.to(device),
+                y_labels=y_labels.to(device)
             )
 
             # optimize
             logits = logits.view(args.batch_size*args.max_len, -1)
             y_labels = y_labels.view(args.batch_size*args.max_len)
-            loss = loss_function(logits, y_labels)
+            loss = loss_function(logits, y_labels.to(device))
             loss.backward()
             optimizer.step()
             lr_scheduler.step()
             
             # metrics
-            auc, precision, recall, f1_score, accuracy = cal_metrics(logits, y_labels)
+            auc, precision, recall, f1_score, accuracy = cal_metrics(logits.cpu().detach(), y_labels.cpu().detach())
 
-            print('In [{}/{}] epoch, [{}/{}] batch, loss:{}, auc:{}, precision:{}, recall:{}, f1_score:{}, accuracy:{}'.format(epoch, args.epoch, batch_id, len(dataset)//args.batch_size, loss, auc, precision, recall, f1_score, accuracy))
+            print('In [{}/{}] epoch, [{}/{}] batch, loss:{}, auc:{}, precision:{}, recall:{}, f1_score:{}, accuracy:{}, --{}'.format(epoch, args.epoch, batch_id, len(dataset)//args.batch_size, loss, auc, precision, recall, f1_score, accuracy, datetime.datetime.now()))
 
 
 if __name__ == '__main__':
@@ -165,10 +180,14 @@ if __name__ == '__main__':
     parser.add_argument('--learning_rate', type=float, default=1e-3)
     parser.add_argument('--warmup_rate', type=float, default=0.03)
     parser.add_argument('--ignore_label', type=int, default=-1)
-
-    parser.add_argument('--data_dir', type=str, default=duolingo_en_es_dir)
+    
+    # paths
+    parser.add_argument('--raw_data_dir', type=str, default=duolingo_raw_en_es_dir)
+    parser.add_argument('--processed_data_dir', type=str, default=duolingo_processed_en_es_dir)
     parser.add_argument('--data_file', type=str, default=file_paths['Duolingo_en_es_train'])
-    parser.add_argument('--vocab_save_path', type=str, default=os.path.join(duolingo_en_es_dir, file_paths['vocab']))
+    parser.add_argument('--vocab_save_path', type=str, default=os.path.join(duolingo_processed_en_es_dir, file_paths['vocab']))
+    parser.add_argument('--model_save_dir', type=str, default=duolingo_en_es_models_dir)
+    
     
     args = parser.parse_args()
 
